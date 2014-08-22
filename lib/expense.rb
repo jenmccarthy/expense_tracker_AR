d360e7947a14a269d2a93750fe1d90c8b5ad5897
jennifermccarthy <jennifernward@gmail.com>
2014-08-22 22:01:16 +0000

    callback capitalize desctiption

--- lib/expense.rb
+++ lib/expense.rb
@@ -3,4 +3,12 @@ class Expense < ActiveRecord::Base
   validates :description, presence: true
   validates :price, presence: true
   
+  before_save :capital_case
+
+private
+
+  def capital_case
+    self.description.capitalize!
+  end
+  
 end
--- spec/expense_spec.rb
+++ spec/expense_spec.rb
@@ -8,4 +8,9 @@ describe Expense do
   it { should validate_presence_of :description }
   it { should validate_presence_of :price }
   
+  it 'converts the description to capital case' do
+    expense = Expense.create({description: 'pizza', price: 4.99})
+    expect(expense.description).to eq 'Pizza'
+  end
+  
 end
